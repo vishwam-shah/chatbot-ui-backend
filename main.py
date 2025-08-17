1f23f3957ae064085cc0fec4808621cf1b9ffc80
Vishwam Shah <vishwamshah007@gmail.com>
2025-08-17 21:05:41 +0530

    updated server

--- main.py
+++ main.py
@@ -10,13 +10,9 @@ from datetime import datetime, timedelta
 
 security = HTTPBearer()
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://chatbot-ui-frontend.vercel.app",
-        "http://localhost:3000"
-    ],
+    allow_origins=["https://chatbot-ui-frontend.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
